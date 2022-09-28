a0f8b7b7a08f263bf62a112f38e9cee67af7c93c
Eduardo Garcia <eduardomgarcia335@gmail.com>
2022-09-28 15:04:34 -0300

    iniciando projeto

---
+++ src/database/migrations/1664387842928-User.ts
@@ -0,0 +1,66 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class User1664387842928 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: `uuid_generate_v4()`,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'birth_date',
+            type: 'date',
+            isNullable: false,
+          },
+          {
+            name: 'cpf',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'observations',
+            type: 'varchar',
+            isNullable: true,
+          },
+          {
+            name: 'permission',
+            type: 'varchar',
+            isNullable: false,
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('users');
+  }
+}
